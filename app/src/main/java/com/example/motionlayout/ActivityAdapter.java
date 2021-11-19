d0d68e4372ad31ee8502e6150fc3c3b08bbfbb05
corn1200 <corn120033@gmail.com>
2021-11-19 11:07:22 +0900

    feat: Activity 클래스를 직접 다루는 대신 feat: ActivityItem 객체를 다루도록 수정한다

--- app/src/main/java/com/example/motionlayout/ActivityAdapter.java
+++ app/src/main/java/com/example/motionlayout/ActivityAdapter.java
@@ -2,37 +2,41 @@ package com.example.motionlayout;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
 public class ActivityAdapter extends RecyclerView.Adapter<ActivityAdapter.ViewHolder> {
-    private Context mContext;
-    private ArrayList<Activity> mActivityData;
 
-    public ActivityAdapter(MainActivity context, ArrayList<Activity> mActivityList) {
+    private Context mContext;
+    private ArrayList<ActivityItem> mActivityData;
+
+    public ActivityAdapter(MainActivity context, ArrayList<ActivityItem> mActivityList) {
         this.mContext = context;
         this.mActivityData = mActivityList;
     }
 
     @NonNull
     @Override
-    public ActivityAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(LayoutInflater.from(mContext)
                 .inflate(R.layout.activity_list_item, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ActivityAdapter.ViewHolder holder, int position) {
-        Activity currentActivity = mActivityData.get(position);
-
-        holder.bindTo(currentActivity);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ActivityItem currentActivityItem = mActivityData.get(position);
+        holder.bindTo(currentActivityItem);
     }
 
     @Override
@@ -41,25 +45,36 @@ public class ActivityAdapter extends RecyclerView.Adapter<ActivityAdapter.ViewHo
     }
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private TextView mTitleText;
+
+        private TextView mNameText;
         private TextView mDescriptionText;
+        private ImageView mThumbnailImage;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            mTitleText = itemView.findViewById(R.id.title);
+            mNameText = itemView.findViewById(R.id.name);
             mDescriptionText = itemView.findViewById(R.id.description);
+            mThumbnailImage = itemView.findViewById(R.id.motionThumbnail);
 
             itemView.setOnClickListener(this);
         }
 
-        public void bindTo(Activity currentActivity) {
-            mTitleText.setText(currentActivity.getTitle());
+        public void bindTo(ActivityItem currentActivityItem) {
+            mNameText.setText(currentActivityItem.getName());
+            mDescriptionText.setText(currentActivityItem.getDescription());
+            Glide.with(mContext).load(
+                    currentActivityItem.getThumbnail()).into(mThumbnailImage);
         }
 
         @Override
         public void onClick(View v) {
-
+            ActivityItem currentActivityItem = mActivityData.get(getAdapterPosition());
+            Intent detailIntent = new Intent(currentActivityItem.getContext(),
+                    DetailMotionActivity.class);
+            detailIntent.putExtra("name", currentActivityItem.getName());
+            mContext.startActivity(detailIntent);
         }
     }
+
 }
