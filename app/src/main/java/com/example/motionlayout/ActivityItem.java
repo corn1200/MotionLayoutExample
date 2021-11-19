f8a2b99aeac7b5c18cac473f5cbb4d469bf2bdd6
corn1200 <corn120033@gmail.com>
2021-11-19 10:23:53 +0900

    feat: MainActivity 에 나열할 액티비티 리스트의 아이템 객체 파일을 추가한다

--- app/src/main/java/com/example/motionlayout/ActivityItem.java
+++ app/src/main/java/com/example/motionlayout/ActivityItem.java
@@ -1,14 +1,33 @@
 package com.example.motionlayout;
 
+import android.content.Context;
+
 public class ActivityItem {
     private String name;
     private String description;
     private final int thumbnail;
+    private Context context;
 
-    ActivityItem(String name, String description, int thumbnail) {
+    ActivityItem(String name, String description, int thumbnail, Context context) {
         this.name = name;
         this.description = description;
         this.thumbnail = thumbnail;
-        
+        this.context = context;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    int getThumbnail() {
+        return thumbnail;
+    }
+
+    Context getContext() {
+        return context;
     }
 }
